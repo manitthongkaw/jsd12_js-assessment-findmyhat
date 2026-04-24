5d6d3c9c12d19b2c9a04b193b4c5d4916b5be3c6
manitthongkaw <manitthongkaw@gmail.com>
2026-04-24 13:53:29 +0700

    feat: add check field

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '🏐';
 const hole = '🚩';
-const fieldCharacter = '🟩';
-const pathCharacter = '🏌️‍♂️';
+const ground = '🟩';
+const player = '🏌️‍♂️';
 
 class Field {
+
   constructor(field) {
     this.field = field;
-    this.areaX = 0;
     this.areaY = 0;
+    this.areaX = 0;
+  }
+
+  checkField() {
+    let move = prompt("Go go go [w = up] [a = down] [s = left] [d = right] : ");
+    while (true) {
+      if (move === "w") {this.areaY -= 1};
+      if (move === "s") {this.areaY += 1};
+      if (move === "a") {this.areaX -= 1};
+      if (move === "d") {this.areaX += 1};
+    }
   }
 }
 
-const map = new Field([
-  [pathCharacter, fieldCharacter, fieldCharacter],
-  [fieldCharacter, hole, fieldCharacter],
-  [fieldCharacter, fieldCharacter, hat]
+const myField = new Field([
+  [player, ground, ground],
+  [ground, hole, ground],
+  [ground, ground, hat]
 ]);
+
+console.log(Field.checkField);
