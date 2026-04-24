693c77dd93a148a4a472a3caa04bd6c34637a858
manitthongkaw <manitthongkaw@gmail.com>
2026-04-24 14:45:15 +0700

    feat: add check move out field

--- index.js
+++ index.js
@@ -14,20 +14,42 @@ class Field {
   }
 
   checkField() {
-    let move = prompt("Go go go [w = up] [a = down] [s = left] [d = right] : ");
+
+    console.log("Height (Y):", this.field.length);
+    console.log("FWidth (X):", this.field[0].length);
+
     while (true) {
+
+      // Check move on field
+      let move = prompt("Go go go [w = up] [a = down] [s = left] [d = right] : ");
       if (move === "w") {this.areaY -= 1};
       if (move === "s") {this.areaY += 1};
       if (move === "a") {this.areaX -= 1};
       if (move === "d") {this.areaX += 1};
+
+      // check move out field
+      if (
+        this.areaY < 0 || this.areaY >= this.field.length ||
+        this.areaX < 0 || this.areaX >= this.field[0].length
+      ) {
+        console.log('✘ ออกนอกแผนที่!');
+        break;
+      }
+
+      // check move to hole
+
+      // check move to hat
+
     }
   }
+
 }
 
 const myField = new Field([
   [player, ground, ground],
   [ground, hole, ground],
-  [ground, ground, hat]
+  [ground, ground, hat],
+  [ground, ground, hole]
 ]);
 
-console.log(Field.checkField);
+myField.checkField();
